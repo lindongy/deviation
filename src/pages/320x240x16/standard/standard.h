f29da499f7ecfb1bb494d9e22947bac7622c4bb2
Yan Lindong <1399196749@qq.com>
2018-03-25 21:00:43 +0800

    更改默认编译devo10固件。

--- src/pages/320x240x16/standard/standard.h
+++
@@ -1,9 +0,0 @@
-#ifndef _STANDARD_H_
-#define _STANDARD_H_
-#include "../../common/standard/common_standard.h"
-
-int STDMIX_ScrollCB(guiObject_t *parent, u8 pos, s8 direction, void *data);
-#define ENTRIES_BY_SCREENSIZE (LCD_WIDTH == 480? 9 : 8) // 320x240: 8, 480x272: 9
-#define ENTRIES_PER_PAGE      (ENTRIES_BY_SCREENSIZE > NUM_CHANNELS ? NUM_CHANNELS : ENTRIES_BY_SCREENSIZE)
-
-#endif
